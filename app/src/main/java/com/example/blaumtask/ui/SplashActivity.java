6b7a6afff663896e9d9985fb3283d64508056529
omarhatem1995 <omar.hatem1230@gmail.com>
2021-04-27 04:15:24 +0200

    added splash screen

---
+++ app/src/main/java/com/example/blaumtask/ui/SplashActivity.java
@@ -0,0 +1,42 @@
+package com.example.blaumtask.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.example.blaumtask.R;
+
+public class SplashActivity extends AppCompatActivity {
+    private ImageView gif;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        initViews();
+        handleGif(MainActivity.class,2500,"splash");
+    }
+
+    private void initViews(){
+        gif = findViewById(R.id.splash_gif);
+        Glide.with(this).asGif().load(R.raw.cosmetics_splash_screen).into(gif);
+
+    }
+
+    public void handleGif(Class activity, long delay, String extras){
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(this,activity);
+            Bundle extra = new Bundle();
+            extra.putString("activity", extras);
+            intent.putExtras(extra);
+            startActivity(intent);
+            finish();
+        },delay);
+    }
+}
